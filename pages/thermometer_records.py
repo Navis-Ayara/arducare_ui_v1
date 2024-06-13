a5c3987374ca024a5ba5d7d5513554cf7208fa17
Navis Ayara <navisayara@gmail.com>
2024-06-13 16:24:04 +0300

    modified

--- pages/thermometer_records.py
+++ pages/thermometer_records.py
@@ -1,17 +1,20 @@
 import flet as ft
 
 
-class SessionView(ft.View):
+class ThermometerView(ft.View):
     def __init__(self, page: ft.Page):
         super().__init__()
 
         self.page = page
 
-        self.route = "/"
+        self.route = "/thermometer-records"
 
     def build(self):
         self.appbar = ft.AppBar(
             elevation=0,
             elevation_on_scroll=0,
-            center_title=True
+            center_title=True,
+            title=ft.Text(
+                "Thermometer data"
+            ),
         )
