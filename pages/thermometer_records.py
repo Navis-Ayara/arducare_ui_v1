7ed9ed3a7c7a81ca972b22c9ee42ad81962cf58c
Navis Ayara <navisayara@gmail.com>
2024-06-18 08:27:17 +0300

    implemented UI design of the thermometer sensor view

--- pages/thermometer_records.py
+++ pages/thermometer_records.py
@@ -7,12 +7,16 @@ class ThermometerView(ft.View):
 
         self.page = page
 
+        self.curr_temp = 36
+
         self.route = "/thermometer-records"
-        self.padding = 0
+        self.padding = ft.padding.only(left=10, right=10)
         self.scroll = ft.ScrollMode.AUTO
+        self.bgcolor = ft.colors.GREEN_ACCENT if self.curr_temp < 40 else ft.colors.RED_ACCENT
 
     def build(self):
         self.appbar = ft.AppBar(
+            bgcolor = ft.colors.GREEN_ACCENT if self.curr_temp < 40 else ft.colors.RED_ACCENT,
             toolbar_height=100,
             elevation=0,
             elevation_on_scroll=0,
@@ -35,30 +39,23 @@ class ThermometerView(ft.View):
         )
 
         self.controls = [
-            ft.Stack([
-                ft.Container(
-                    bgcolor=ft.colors.GREEN_ACCENT,
-                    expand=True
-                ),
-                ft.Column([
-                    ft.Container(
-                        height=self.page.window_height,
-                        alignment=ft.alignment.center,
-                        content=ft.Text(
-                            value="37°C",
-                            size=48,
-                            weight=ft.FontWeight.BOLD
-                        )
-                    ),
-                    ft.ListView([
-                        ft.ListTile(
-                            height=140,
-                            on_click=lambda _: print("Loading Record..."),
-                            title=ft.Text(
-                                value="Locked"
-                            )
-                        )
-                    for i in range(7)])
-                ])
-            ])
+            ft.Container(
+                height=self.page.window_height,
+                alignment=ft.alignment.center,
+                content=ft.Text(
+                    value=f"{self.curr_temp}°C",
+                    size=48,
+                    weight=ft.FontWeight.BOLD
+                )
+            ),
+            ft.ListView([
+                ft.ListTile(
+                    hover_color=ft.colors.with_opacity(0.4, ft.colors.GREY),
+                    height=140,
+                    on_click=lambda _: print("Loading Record..."),
+                    title=ft.Text(
+                        value="Locked"
+                    )
+                )
+            for i in range(7)])
         ]
